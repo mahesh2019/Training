1a8584f3e45a6c62c08d1f6e8acc06534dfc9214
mahesh2019 <mahesh281420@gmail.com>
2019-02-01 19:15:42 +0530

    large_sample

--- python/code_to_add.py
+++ python/code_to_add.py
@@ -3,72 +3,30 @@ uri             = "bolt://localhost:7687"
 
 userName        = "neo4j"
 
-password        = "password"
+password        = "mahesh@1994"
 
 graphDB_Driver  = GraphDatabase.driver(uri, auth=(userName, password)) 
 
-cqlNodeQuery          = "MATCH (x:Family) RETURN x"
-
-cqlEdgeQuery          = "MATCH (x:Family {name:'jack'})-[r]->(y:Family) RETURN y.name,r.Relation"
-
-cqlCreate = """CREATE (Ron:Family{ name: "Ron"}),
-
-(Jack:Family { name: "Jack"}),
-
-(Hermonine:Family { name: "Hermonine"}),
-
-(Rose:Family { name: "Rose"}),
-
- 
-
-(Ron)-[:Father{Relation:"Father"}]->(Rose),
-
-(Ron)-[:Father {Relation:"Father"}]->(Jack),
-
-(Ron)-[:Husband {Relation:"Husband"}]->(Hermonine),
-
- 
-
-(Hermonine)-[:Mother {Relation:"Mother"}]->(Rose),
-
-(Hermonine)-[:Mother {Relation:"Mother"}]->(Jack),
-
-(Hermonine)-[:Wife {Relation:"Wife"}]->(Ron),
-
- 
-
-(Jack)-[:Son {Relation:"son"}]->(Ron),
-
-(Jack)-[:Son {Relation:"son"}]->(Hermonine),
-
-(Jack)-[:Brother {Relation:"Brother"}]->(Rose),
 
 
-(Rose)-[:Daughter {Relation:"Daughter"}]->(Ron),
+name=['rose','jack','ron','hermonine']
 
-(Rose)-[:Daughter {Relation:"Daughter"}]->(Hermonine),
+for i in name:
+  query="create("+i+":family{name:" '"'+i+'"' "})"
+  print(query)
+  with graphDB_Driver.session() as graphDB_Session:
+    graphDB_Session.run(query)
 
-(Rose)-[:Sister {Relation:"Sister"}]->(Jack)"""
 
 
-with graphDB_Driver.session() as graphDB_Session:
 
-    graphDB_Session.run(cqlCreate)
 
-       
-    nodes = graphDB_Session.run(cqlNodeQuery)
 
-    print("List of Ivy League universities present in the graph:")
 
-    for node in nodes:
 
-        print(node)
 
-    nodes = graphDB_Session.run(cqlEdgeQuery)
 
-    print("Distance")
 
-    for node in nodes:
 
-        print(node)
+    
 
