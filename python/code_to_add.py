82011f34947c6fc2ead0eb84e9705014dd79f339
mahesh2019 <mahesh281420@gmail.com>
2019-02-06 12:30:20 +0530

    pep_edit

--- python/code_to_add.py
+++ python/code_to_add.py
@@ -1,25 +1,17 @@
 from neo4j.v1 import GraphDatabase
-uri             = "bolt://172.17.2.109:7687"
+uri = "bolt://172.17.2.109:7687"
 
-userName        = "neo4j"
+userName = "neo4j"
 
-password        = "PILLOWS112012;"
+password = "PILLOWS112012;"
 
-graphDB_Driver  = GraphDatabase.driver(uri, auth=(userName, password)) 
-
-
-
-name=['rose','jack','ron','hermonine']
-
+graphDB_Driver = GraphDatabase.driver(uri, auth=(userName, password))
+name = ['rose', 'jack', 'ron', 'hermonine']
 for i in name:
-  query="create("+i+":family{name:" '"'+i+'"' "})"
-  print(query)
-  with graphDB_Driver.session() as graphDB_Session:
-    graphDB_Session.run(query)
-
-
-
-
+    query = "create("+i+":family{name:" '"'+i+'"' "})"
+    print(query)
+    with graphDB_Driver.session() as graphDB_Session:
+        graphDB_Session.run(query)
 
 
 
